ab41ea23f0a4588cabb21a876089e35c65565009
priyadixit123 <48387221+priyadixit123@users.noreply.github.com>
2025-05-05 12:14:17 +0530

    175. Combine Two Tables.sql

---
+++ 175. Combine Two Tables.sql
@@ -0,0 +1,17 @@
+https://leetcode.com/problems/combine-two-tables/description/
+
+Sol: SQL
+
+
+
+SELECT 
+    p.firstName, 
+    p.lastName, 
+    a.city, 
+    a.state
+FROM 
+    Person p
+LEFT JOIN 
+    Address a
+ON 
+    p.personId = a.personId;
