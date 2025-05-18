f7ee43d17fd19ac9c38fe47ccaedbeba67c608a5
priyadixit123 <48387221+priyadixit123@users.noreply.github.com>
2025-05-18 08:05:05 +0530

    185. Department Top Three Salaries.sql

---
+++ 185. Department Top Three Salaries.sql
@@ -0,0 +1,12 @@
+https://leetcode.com/problems/department-top-three-salaries/description/
+
+Sol:
+
+SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
+FROM (
+    SELECT *,
+           DENSE_RANK() OVER (PARTITION BY departmentId ORDER BY salary DESC) AS salary_rank
+    FROM Employee
+) e
+JOIN Department d ON e.departmentId = d.id
+WHERE e.salary_rank <= 3;
