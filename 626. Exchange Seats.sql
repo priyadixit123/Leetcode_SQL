30b58410fff2f3b2070daea626dd49372b87cc77
priyadixit123 <48387221+priyadixit123@users.noreply.github.com>
2025-06-05 13:36:24 +0530

    626. Exchange Seats.sql

---
+++ 626. Exchange Seats.sql
@@ -0,0 +1,13 @@
+https://leetcode.com/problems/exchange-seats/description/
+
+Sol:
+
+SELECT 
+    CASE
+        WHEN id % 2 = 1 AND id + 1 <= (SELECT COUNT(*) FROM Seat) THEN id + 1
+        WHEN id % 2 = 0 THEN id - 1
+        ELSE id
+    END AS id,
+    student
+FROM Seat
+ORDER BY id;
